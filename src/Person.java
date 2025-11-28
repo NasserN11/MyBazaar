04c0fba7fea35d9241b7585ac341d34505b60340
Nasser <nasser.mahamat.swe@gmail.com>
2025-11-28 22:18:36 +0300

    Completed Person class with displayPersonalData and toString methods

--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,10 @@
-public class Person {
+public abstract class Person {
 
+    // Private attributes
     private String name;
     private String email;
     private String dateOfBirth;
+
 
     // Constructor
     public Person(String name, String email, String dateOfBirth) {
@@ -11,10 +13,23 @@ public class Person {
         this.dateOfBirth = dateOfBirth;
     }
 
+
     // Getters
     public String getName() { return name; }
 
     public String getEmail() { return email; }
 
     public String getDateOfBirth() { return dateOfBirth; }
+
+
+    // Methods
+    public String toString() {
+        return "name: " + name + ", email: " + email + "date of birth: " + dateOfBirth;
+    }
+
+    public void displayPersonalData() {
+        System.out.println("name: " + name);
+        System.out.println("e-mail: " + email);
+        System.out.println("date of birth: " + dateOfBirth);
+    }
 }
